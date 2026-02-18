dd687af3015808aa5679072ff3e8f81a8e8bedab
Pravin Nagre <pravinnagre312@gmail.com>
2026-02-18 10:05:55 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+ import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1>hello..! Wellcome to React program</h1>
+
+      <p> We are lerning hear react html css javascript bootstrap monogo node.js express.js  SQL </p>
+    </div>
+  )
+}
+
+export default App
